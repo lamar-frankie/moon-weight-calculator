5c2e62736f631445b6bb55ab1e7efa3b1c068299
Frank Lamar <lamar-frankie@users.noreply.github.com>
2021-01-13 05:17:20 +0000

    WIP: test not passing

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ def test_exists():
 def test_runnable():
     """Runs using python3"""
 
-    out = getoutput(f'python3 {prg}')
+    out = getoutput(f' python3 {prg}')
     assert out.strip() == 'Hello, World!'
 
 #--------------------------------------
@@ -25,3 +25,21 @@ def test_executable():
 
     out = getoutput(prg)
     assert out.strip() == 'Hello, World!'
+
+#------------------------------------------
+def test_usage():
+    """usage"""
+
+    for flag in ['-h', '--help']:
+        out = getstatusoutput(f'{prg}{flag}')
+        #assert rv == 0
+        assert out.lower().startswith('usage')
+
+#------------------------------------------
+def test_input():
+    """test for input"""
+
+    for val in ['Universe', 'Multiverse']:
+        for option in ['-n', '--name']:
+            out = getstatusoutput(f'{prg}{option}{val}')
+            assert out == f'Hello, {val}!'
