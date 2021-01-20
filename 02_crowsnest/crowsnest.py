ef8c01d651828864e39b35cb6979ff044cdbb10d
Frank Lamar <lamar-frankie@users.noreply.github.com>
2021-01-20 03:15:31 +0000

    WIP: creating help arguments

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
+"""
+Author : Frank Lamar
+Date   : 01/19/2021
+Purpose: Print out a pirate message
+"""
 
 import argparse
 
 def get_args():
-    parser = argparse.Ar
+    parser = argparse.ArgumentParser(description='Print out a Pirate Message')
+
+    parser.add_argument('-h', '--help', help='Print a Pirate message')
+
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    print(args)
+
+if __name__ == '__main__':
+    main() 
